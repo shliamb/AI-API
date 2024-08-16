c5e31904d24de6f26680d3ab79bbf769ac0b5e4b
Shaposhnikov Alex <firstremont@gmail.com>
2024-08-16 12:50:23 +0300

    work1

--- app/keys.py
+++ app/keys.py
@@ -5,9 +5,3 @@ load_dotenv()
 token_telegram = os.environ.get('TELEGRAM_BOT_CHATGPT_API_KEY') # Telegram key
 api_key_openai = os.environ.get('CHATGPT_API_KEY') # OpenAI key-token - установить только на сервере руками
 my_key = os.environ.get('MY_KEY')
-# white_list = os.environ.get('WHITE_LIST')
-# admin_user_ids = os.environ.get('ADMIN_USER_IDS')
-# block = os.environ.get('ALLOWED_TELEGRAM_USER_IDS')
-# oppas = os.environ.get('OPPAS')
-# user_db = os.environ.get('USER_DB')
-# paswor_db = os.environ.get('PASWOR_DB')
--- app/run.py
+++ app/run.py
@@ -1,24 +1,39 @@
 import asyncio
 from openai import AsyncOpenAI, RateLimitError, OpenAIError
-from fastapi import FastAPI, Query, HTTPException
+from fastapi import FastAPI, Header, Depends, HTTPException, status
 from pydantic import BaseModel
 from keys import api_key_openai, my_key
 
 client = AsyncOpenAI(api_key=api_key_openai)
-
 app = FastAPI()
 
 
-# Модель запроса для получения текста от пользователя
+
+# Checking the api_key user
+def verify_api_key(api_key: str = Header(...)):
+    if api_key != my_key:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Invalid API Key",
+        )
+
+# Model Text Chat GPT
 class UserInput(BaseModel):
     prompt: str
 
-@app.post("/chat/")
-async def chat(user_input: UserInput, api_key: str = Query(...)):
+# Endpoint Text Chat GPT
+@app.post("/chat/", status_code=status.HTTP_201_CREATED)
+async def chat(user_input: UserInput, api_key: str = Depends(verify_api_key)):
 
-    # Проверка api_key
-    if api_key != my_key:
-        raise HTTPException(status_code=403, detail="Forbidden")
+
+    '''
+    API Sample Question:
+        {
+            "prompt": "Как ты бро?",
+            "model": "gpt-4o-mini-2024-07-18",
+
+        }
+    '''
 
     try:
         chat_completion = await client.chat.completions.create(
@@ -41,7 +56,12 @@ async def chat(user_input: UserInput, api_key: str = Query(...)):
     except OpenAIError as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+
+
+
+
+
 if __name__ == "__main__":
     import uvicorn
     
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000) # При деплое переделать
