ef998dc77a1adb3984d1d211c6410c5799ee6c21
Shaposhnikov Alex <firstremont@gmail.com>
2024-08-30 13:08:42 +0300

    rename repository

--- app/models.py
+++ app/models.py
@@ -20,7 +20,7 @@ Column = sqlalchemy.Column
 
 
 
-# Users Main
+# Users Main Data
 class UsersBase(Base):
     __tablename__ = 'users'
     id = Column(sqlalchemy.BigInteger, primary_key=True, unique=True, nullable=False, index=True) # Telegram id
@@ -28,24 +28,25 @@ class UsersBase(Base):
     full_name = Column(sqlalchemy.String(50), nullable=True) # Telegram
     first_name = Column(sqlalchemy.String(50), nullable=True) # Telegram
     last_name = Column(sqlalchemy.String(50), nullable=True) # Telegram
-    username = Column(sqlalchemy.String(50), nullable=True) # Username пользователя, для идентификации в API
-    appkey = Column(UUID(as_uuid=True), default=uuid.uuid4, nullable=False) # Автоматически создается UUID в качестве API Key
+
+    username = Column(sqlalchemy.String(50), nullable=False) # Username пользователя, для идентификации в API
+    appkey = Column(UUID(as_uuid=True), default=uuid.uuid4, nullable=False) # Автоматически создается UUID в качестве API Key - 9a5bce5f-4462-4d12-a66c-d59584b19ee8
+    is_failed = Column(sqlalchemy.Integer(50), nullable=False, default=0, server_default="0" ) # Неудачные попытки доступа
     is_block = Column(sqlalchemy.Boolean, default=False, server_default="False", nullable=False) # Заблокирован ли пользователь из-за колл. не верных вводов.
     date_block = Column(sqlalchemy.DateTime, nullable=True) # Дата и время блокировки, для отсчета времени блокировки
-    cash = Column(sqlalchemy.Float, default=0, server_default="0", nullable=False)
-    money_currency = Column(sqlalchemy.String(10), default="RUB", server_default="RUB", nullable=False)
+    money = Column(sqlalchemy.Float, default=0, server_default="0", nullable=False) # Денег на счете в $, пезволит унифицироваться и выстроить путь "идеального" приложения
+    money_currency = Column(sqlalchemy.String(10), default="USD", server_default="USD", nullable=False) # Сразу все в $, так проще, раньше все проги так и работали, возвращаемся))
     ###
 
-    #"9a5bce5f-4462-4d12-a66c-d59584b19ee8",
 
-
-
+# Exchange Data - будут строки с валютами и их курс по отношению к $, пока что только RUB, но можно будет и другие, посмотрим. Курс будет нужен для ввода старонней валюты, отличной от $, но внутренняя валюта будет только $
 class Exchange(Base):
     __tablename__ = 'exchange'
     id = Column(sqlalchemy.Integer, primary_key=True)
-    date = Column(sqlalchemy.DateTime, nullable=True)
-    rate = Column(sqlalchemy.Float(), default=100.0, server_default="100.0", nullable=False)
-    currency = Column(sqlalchemy.String(10), default="USDT", server_default="USDT", nullable=False)
+    date = Column(sqlalchemy.DateTime, nullable=True) # Дата обновления курса
+    rate = Column(sqlalchemy.Float(), nullable=True) # Курс валюты к $
+    currency = Column(sqlalchemy.String(10), nullable=True) # Внутренее обозначение или название валюты, например RUB
+    counter = Column(sqlalchemy.BigInteger, nullable=True) # Возможно я захочу собирать статистику обмена
     ###
 
 class Backup(Base):
