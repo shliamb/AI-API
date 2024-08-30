179813f20be815c4ba0bce307b225c64c09a8089
Shaposhnikov Alex <firstremont@gmail.com>
2024-08-30 15:29:25 +0300

    orm start

--- app/models.py
+++ app/models.py
@@ -9,8 +9,13 @@ import uuid
 from sqlalchemy import ForeignKey
 import sqlalchemy
 from typing import AsyncGenerator
-from keys import user_db, paswor_db
+#from keys import user_db, paswor_db
 
+
+import os
+from dotenv import load_dotenv
+load_dotenv(os.path.join(os.path.dirname(__file__), '..', '.env'))
+user_db, paswor_db = os.environ.get('USER_DB'),  os.environ.get('PASWOR_DB')
 
                                                             # @localhost  # @postgres
 DATABASE_URL = f"postgresql+asyncpg://{user_db}:{paswor_db}@localhost:5432/my_database"
@@ -23,20 +28,36 @@ Column = sqlalchemy.Column
 # Users Main Data
 class UsersBase(Base):
     __tablename__ = 'users'
+    # Telegram
     id = Column(sqlalchemy.BigInteger, primary_key=True, unique=True, nullable=False, index=True) # Telegram id
     name = Column(sqlalchemy.String(50), nullable=True) # Telegram
     full_name = Column(sqlalchemy.String(50), nullable=True) # Telegram
     first_name = Column(sqlalchemy.String(50), nullable=True) # Telegram
     last_name = Column(sqlalchemy.String(50), nullable=True) # Telegram
-
+    # User data
     username = Column(sqlalchemy.String(50), nullable=False) # Username пользователя, для идентификации в API
     appkey = Column(UUID(as_uuid=True), default=uuid.uuid4, nullable=False) # Автоматически создается UUID в качестве API Key - 9a5bce5f-4462-4d12-a66c-d59584b19ee8
-    is_failed = Column(sqlalchemy.Integer(50), nullable=False, default=0, server_default="0" ) # Неудачные попытки доступа
+    is_failed = Column(sqlalchemy.Integer(), nullable=False, default=0, server_default="0" ) # Неудачные попытки доступа
     is_block = Column(sqlalchemy.Boolean, default=False, server_default="False", nullable=False) # Заблокирован ли пользователь из-за колл. не верных вводов.
     date_block = Column(sqlalchemy.DateTime, nullable=True) # Дата и время блокировки, для отсчета времени блокировки
     money = Column(sqlalchemy.Float, default=0, server_default="0", nullable=False) # Денег на счете в $, пезволит унифицироваться и выстроить путь "идеального" приложения
     money_currency = Column(sqlalchemy.String(10), default="USD", server_default="USD", nullable=False) # Сразу все в $, так проще, раньше все проги так и работали, возвращаемся))
+    # Stat
+    stat = relationship("Statistics")
+
     ###
+
+# User spending statistics - One-to-many
+class Statistics(Base):
+    __tablename__ = 'statistics'
+    id = Column(sqlalchemy.Integer, primary_key=True)
+    time = Column(sqlalchemy.DateTime, nullable=True) 
+    use_model = Column(sqlalchemy.String(50), nullable=False) # Использованная модель
+    sesion_token = Column(sqlalchemy.Integer, default=0, server_default="0",  nullable=False) # Использованно токенов
+    price_1_tok = Column(sqlalchemy.Float, default=0, server_default="0", nullable=False) # Цена токена, если менялась, то будет видно
+    price_sesion_tok = Column(sqlalchemy.Float, default=0, server_default="0", nullable=False) #  Вся цена
+    ###
+    users_telegram_id = Column(sqlalchemy.BigInteger, ForeignKey("users.id"), index=True) # id user telegram
 
 
 # Exchange Data - будут строки с валютами и их курс по отношению к $, пока что только RUB, но можно будет и другие, посмотрим. Курс будет нужен для ввода старонней валюты, отличной от $, но внутренняя валюта будет только $
