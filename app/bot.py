721ae13531b8ed4d0d53c5ccfbd2f97d1ccc1933
Shaposhnikov Alex <firstremont@gmail.com>
2024-09-03 19:43:55 +0300

    zaebali logi

--- app/bot.py
+++ app/bot.py
@@ -1,7 +1,7 @@
 # Base
 import logging
-logging.getLogger('aiogram').propagate = True # Блокировка логирование aiogram до его импорта
-logging.basicConfig(level=logging.INFO, filename='log/app.log', filemode='a', format='%(levelname)s - %(asctime)s - %(name)s - %(message)s',) # При деплое активировать логирование в файл
+# logging.getLogger('aiogram').propagate = True # Блокировка логирование aiogram до его импорта
+# logging.basicConfig(level=logging.INFO, filename='log/app.log', filemode='a', format='%(levelname)s - %(asctime)s - %(name)s - %(message)s',) # При деплое активировать логирование в файл
 import re
 import random
 import os
