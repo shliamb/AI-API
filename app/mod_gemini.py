adc51e06bd600f21708b4df80f34497d79e6f480
Shaposhnikov Alex <firstremont@gmail.com>
2024-09-05 23:26:22 +0300

    gemini-end

--- app/mod_gemini.py
+++ app/mod_gemini.py
@@ -11,12 +11,15 @@ genai.configure(api_key=api_key_gemini)
 
 
 async def mod_gemini(username, user_input):
-    model = genai.GenerativeModel(user_input.model) # gemini-1.5-flash-001  "gemini-1.5-flash"
+    model = genai.GenerativeModel(model_name=user_input.model, tools=user_input.tools) # gemini-1.5-flash-001  "gemini-1.5-flash"
+    # organ = PIL.Image.open(media / "organ.jpg")
     response = model.generate_content(user_input.user_content)
     print(response.text)
     return {"response": response.text}
 
 
+# # Set the `response_mime_type` to output JSON
+# generation_config={"response_mime_type": "application/json"})
 
 
 
