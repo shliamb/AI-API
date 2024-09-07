c7d7fd414ba7d439e6ac98cf6e34aa8ec7d26e5f
Shaposhnikov Alex <firstremont@gmail.com>
2024-09-08 01:32:00 +0300

    defdf

--- app/mod_openai_image.py
+++ app/mod_openai_image.py
@@ -1,10 +1,11 @@
+from openai import AsyncOpenAI, RateLimitError, OpenAIError
 from openai import OpenAI
 from keys import api_key_openai
 
 
 
-client = OpenAI(api_key=api_key_openai)
-#client = AsyncOpenAI(api_key=api_key_openai)
+#client = OpenAI(api_key=api_key_openai)
+client = AsyncOpenAI(api_key=api_key_openai)
 
 prompt = "A cute baby sea otter"
 model = "dall-e-3"
@@ -12,11 +13,11 @@ model = "dall-e-3"
 
 async def mod_dall_e(username, description):
     # Generate an image based on the prompt
-    response = client.images.generate(
+    response = await client.images.generate(
         prompt=prompt,
         model=model,
-        size="1024x1024",
-        n=1,
+        size="512×512",  # 1024x1024
+        n=1, # Колличество картинок
     )
 
     # Prints response containing a URL link to image
