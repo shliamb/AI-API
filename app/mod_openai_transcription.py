da665a9f96e1c27df96869597f908e7429b00112
Shaposhnikov Alex <firstremont@gmail.com>
2024-09-16 01:22:47 +0300

    dfd

--- app/mod_openai_transcription.py
+++ app/mod_openai_transcription.py
@@ -9,11 +9,12 @@ client = AsyncOpenAI(api_key=api_key_openai)
 
 
 
-async def read_file_async(audio_path):
-    async with aiofiles.open(audio_path, 'rb') as file:
-        content = await file.read()
-    return content
+# async def read_file_async(audio_path):
+#     async with aiofiles.open(audio_path, 'rb') as file:
+#         content = await file.read()
+#     return content
 
+# content = await read_file_async(audio_path)
 
 
 
@@ -25,20 +26,20 @@ async def transcription_openai(description, audio_path):
     model = description.get("model", "whisper-1") # whisper-1 only now
     response_format = description.get("response_format", "text") # json, text, srt, verbose_json, or vtt
 
-    #async with aiofiles.open(audio_path, "rb") as file:
-        # content = await file.read()  # Читаем содержимое файла
+    async with aiofiles.open(audio_path, "rb") as file:
+        content = await file.read()  # Читаем содержимое файла
     #content = open(audio_path, "rb")
-    content = await read_file_async(audio_path)
 
-    transcript = await client.audio.transcriptions.create(
-        model = model,
-        prompt = prompt,
-        language = language,
-        response_format = response_format,
-        # timestamp_granularities=["word"],
-        # timestamp_granularities=["segment"]
-        file = content
-    )
+
+        transcript = await client.audio.transcriptions.create(
+            model = model,
+            prompt = prompt,
+            language = language,
+            response_format = response_format,
+            # timestamp_granularities=["word"],
+            # timestamp_granularities=["segment"]
+            file = content
+        )
 
     return transcript
 
