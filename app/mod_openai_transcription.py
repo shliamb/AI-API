f7f2a8f936c059479b09b969e9cd509f0ee9214a
Shaposhnikov Alex <firstremont@gmail.com>
2024-09-16 00:41:29 +0300

    dfdfgd

--- app/mod_openai_transcription.py
+++ app/mod_openai_transcription.py
@@ -18,7 +18,7 @@ async def transcription_openai(description, audio_path):
 
     async with aiofiles.open(audio_path, "rb") as audio_file:
 
-        audio_open = await audio_file.read()  # Читаем содержимое файла
+        # audio_open = await audio_file.read()  # Читаем содержимое файла
 
         transcript = await client.audio.transcriptions.create(
             model = model,
@@ -27,7 +27,7 @@ async def transcription_openai(description, audio_path):
             response_format = response_format,
             # timestamp_granularities=["word"],
             # timestamp_granularities=["segment"]
-            file = audio_open
+            file = audio_file
         )
 
     return transcript
