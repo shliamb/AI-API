dfab3fd12d1b202c5ae9b5836ec716a3353a8b6d
Shaposhnikov Alex <firstremont@gmail.com>
2024-09-16 01:18:35 +0300

    fghf

--- app/mod_openai_transcription.py
+++ app/mod_openai_transcription.py
@@ -2,13 +2,19 @@ from pathlib import Path
 from openai import AsyncOpenAI, RateLimitError, OpenAIError
 import aiofiles
 
-# from io import BytesIO
-import io
-
 from keys import api_key_openai
 
 
 client = AsyncOpenAI(api_key=api_key_openai)
+
+
+
+async def read_file_async(audio_path):
+    async with aiofiles.open(audio_path, 'rb') as file:
+        content = await file.read()
+    return content
+
+
 
 
 async def transcription_openai(description, audio_path):
@@ -19,13 +25,10 @@ async def transcription_openai(description, audio_path):
     model = description.get("model", "whisper-1") # whisper-1 only now
     response_format = description.get("response_format", "text") # json, text, srt, verbose_json, or vtt
 
-    async with aiofiles.open(audio_path, "rb") as file:
-        content = await file.read()  # Читаем содержимое файла
-
-    # Оборачиваем байты в BytesIO
-    file_like_object = io.BytesIO(content)
-
-    # content = open(audio_path, "rb")
+    #async with aiofiles.open(audio_path, "rb") as file:
+        # content = await file.read()  # Читаем содержимое файла
+    #content = open(audio_path, "rb")
+    content = await read_file_async(audio_path)
 
     transcript = await client.audio.transcriptions.create(
         model = model,
@@ -34,7 +37,7 @@ async def transcription_openai(description, audio_path):
         response_format = response_format,
         # timestamp_granularities=["word"],
         # timestamp_granularities=["segment"]
-        file = file_like_object,
+        file = content
     )
 
     return transcript
