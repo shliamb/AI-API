10c8233eb10b7f5a3da6bb2df2a2020df94c3795
Shaposhnikov Alex <firstremont@gmail.com>
2024-09-16 14:21:07 +0300

    drtyyyy

--- app/mod_openai_transcription.py
+++ app/mod_openai_transcription.py
@@ -78,9 +78,9 @@ async def transcription_openai(description, audio_file_path):
 
         data = {
             "model": model,
-            # "language": language,
-            # "prompt": prompt,
-            # "response_format": response_format,
+            "language": language,
+            "prompt": prompt,
+            "response_format": response_format,
 
         }
 
@@ -94,7 +94,7 @@ async def transcription_openai(description, audio_file_path):
         else:
             print("Error:", response.status_code, response.text)
 
-    #return response#.json()
+    return response#.json()
 
 
 
