53070f786be304d5b400b5913470e08f22325041
Shaposhnikov Alex <firstremont@gmail.com>
2024-09-16 14:30:28 +0300

    ddere

--- app/mod_openai_transcription.py
+++ app/mod_openai_transcription.py
@@ -88,7 +88,8 @@ async def transcription_openai(description, audio_file_path):
 
         if response.status_code == 200:
             print("Transcription:", response.json())
-            return {"response": response.text}
+            print(type(response))
+            #return {"response": response.text}
         else:
             print("Error:", response.status_code, response.text)
 
