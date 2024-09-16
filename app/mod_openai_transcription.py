93fd1f4b5ac9bef94a351474c04a682344389999
Shaposhnikov Alex <firstremont@gmail.com>
2024-09-16 15:40:46 +0300

    ffdsd

--- app/mod_openai_transcription.py
+++ app/mod_openai_transcription.py
@@ -12,9 +12,7 @@ async def transcription_openai(description, audio_path):
     model = description.get("model", "whisper-1") # whisper-1 only now
     response_format = description.get("response_format", "text") # json, text, srt, verbose_json, or vtt
 
-    async with aiofiles.open(audio_path, "rb") as file:
-
-        content = file._file
+    with open(audio_path, "rb") as file:
 
         transcript = await client.audio.transcriptions.create(
             model = model,
@@ -23,10 +21,10 @@ async def transcription_openai(description, audio_path):
             response_format = response_format,
             # timestamp_granularities=["word"],
             # timestamp_granularities=["segment"]
-            file = content
+            file = file,
         )
 
-        return transcript
+        return {"response":transcript} 
 
 
 
