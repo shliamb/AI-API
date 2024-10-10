794dc75c62a4dfeaba270144f147577c3d995612
Shaposhnikov Alex <firstremont@gmail.com>
2024-10-10 23:38:18 +0300

    dddsdsds

--- app/mod_gemini_main.py
+++ app/mod_gemini_main.py
@@ -52,25 +52,29 @@ async def mod_gemini(description, image_path):
         data["system_instruction"] = {"parts": {"text": system_content},}
 
 
-    async with aiohttp.ClientSession() as session:
-        async with session.post(url, json=data, headers=headers) as response:
-            response = await response.json()
+    print(data)
+
+    # async with aiohttp.ClientSession() as session:
+    #     async with session.post(url, json=data, headers=headers) as response:
+    #         response = await response.json()
+
+
 
             # Tokens:
-            if response:
-                response_text = response['candidates'][0]['content']['parts'][0]['text']
-                total_token_count = response['usageMetadata']['totalTokenCount'] # totalTokenCount - это все токены и на входе и на выходе.
-            else:
-                logging.error("No response from Google Gemini.")
-                return {"response": "No response from Google Gemini."}
+            # if response:
+            #     response_text = response['candidates'][0]['content']['parts'][0]['text']
+            #     total_token_count = response['usageMetadata']['totalTokenCount'] # totalTokenCount - это все токены и на входе и на выходе.
+            # else:
+            #     logging.error("No response from Google Gemini.")
+            #     return {"response": "No response from Google Gemini."}
 
-            model_version = model_name
-            used_tokens = total_token_count
+            # model_version = model_name
+            # used_tokens = total_token_count
 
-            # Calculation of money spent on tokens
-            expenses = await calculation(username, model_version, used_tokens, input_data="text")
+            # # Calculation of money spent on tokens
+            # expenses = await calculation(username, model_version, used_tokens, input_data="text")
 
-            return {"response": response_text, "expenses": expenses, "used_tokens": used_tokens}
+            # return {"response": response_text, "expenses": expenses, "used_tokens": used_tokens}
 
 
 
