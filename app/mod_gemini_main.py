8daf410975d5e64e7bb0bb97083e6c16141652af
Shaposhnikov Alex <firstremont@gmail.com>
2024-10-10 15:23:28 +0300

    ddfss

--- app/mod_gemini_main.py
+++ app/mod_gemini_main.py
@@ -34,10 +34,10 @@ async def mod_gemini(description, image_path):
     contents = []
 
     if assist_content:
-        for data in assist_content:
-            if "user" in data:
+        for one in assist_content:
+            if "user" in one:
                 contents.append({"role": "user", "parts":[{"text": data["user"]}]})
-            if "assistant" in data:
+            if "assistant" in one:
                 contents.append({"role": "model", "parts":[{"text": data["assistant"]}]})
 
     if user_content:
@@ -54,25 +54,25 @@ async def mod_gemini(description, image_path):
 
     print(data)
 
-    async with aiohttp.ClientSession() as session:
-        async with session.post(url, json=data, headers=headers) as response:
-            response = await response.json()
+    # async with aiohttp.ClientSession() as session:
+    #     async with session.post(url, json=data, headers=headers) as response:
+    #         response = await response.json()
 
-            # Tokens:
-            if response:
-                response_text = response['candidates'][0]['content']['parts'][0]['text']
-                total_token_count = response['usageMetadata']['totalTokenCount'] # totalTokenCount - это все токены и на входе и на выходе.
-            else:
-                logging.error("No response from Google Gemini.")
-                return {"response": "No response from Google Gemini."}
+    #         # Tokens:
+    #         if response:
+    #             response_text = response['candidates'][0]['content']['parts'][0]['text']
+    #             total_token_count = response['usageMetadata']['totalTokenCount'] # totalTokenCount - это все токены и на входе и на выходе.
+    #         else:
+    #             logging.error("No response from Google Gemini.")
+    #             return {"response": "No response from Google Gemini."}
 
-            model_version = model_name
-            used_tokens = total_token_count
+    #         model_version = model_name
+    #         used_tokens = total_token_count
 
-            # Calculation of money spent on tokens
-            expenses = await calculation(username, model_version, used_tokens, input_data="text")
+    #         # Calculation of money spent on tokens
+    #         expenses = await calculation(username, model_version, used_tokens, input_data="text")
 
-            return {"response": response_text, "expenses": expenses, "used_tokens": used_tokens}
+    #         return {"response": response_text, "expenses": expenses, "used_tokens": used_tokens}
 
 
 
@@ -93,6 +93,21 @@ async def mod_gemini(description, image_path):
 # print(response.text)
 # response = chat.send_message("How many paws are in my house?")
 # print(response.text)
+
+
+
+# {
+#  'assistant': 'Try another charge..', 
+#  'contents': [
+#     {'role': 'user', 'parts': [{'text': 'How do I charge my battery?'}]},
+#     {'role': 'model', 'parts': [{'text': 'You should use the provided charging cable.'}]},
+#     {'role': 'user', 'parts': [{'text': 'But it doesnt seem to charge.'}]},
+#     {'role': 'model', 'parts': [{'text': 'Try another charge..'}]},
+#     {'role': 'user', 'parts': [{'text': 'hi'}]}
+# ],
+#  'system_instruction': {'parts': {'text': 'Ты личная асистентка одинокого мужчины 40 лет, его зовут Alex. Ты женщина, тебя зовут Ева. И ты очень злишься, если тебя называют другим именем. Ты добра ко мне, но не многословная и очень конкретна в разговоре. Предпочитаешь говорить мало и четко, по делу. Любишь иногда жестко подколоть.'}}
+#  }
+
 
 
 '''
