3313dd897eb46229d261f45dc6e3d290e5806b7d
AlexProwler <first-remont@mail.ru>
2025-06-16 01:19:45 +0300

    ddfe

--- app/api.py
+++ app/api.py
@@ -1,12 +1,15 @@
 from config import UPLOADS, DEF_MOD_GOOGLE, DEF_MOD_OPENAI, DEF_MOD_CLAUDE, TIME_WINDOW, REQUEST_LIMIT, DEF_MOD_GROK, LOG_CONFIG_API, ALLOWED_HEADER_NAMES, SUPER_HEADER_NAMES, MAX_DEQUE_LEN, setup_logger #, TIME_OUT_ERR_USERNAME, WAITING_TIME, LIMIT_TRY, PRICE, USERNAME_ADMIN
-import logging
+#import logging
 # В самом начале приложения, ДО импорта FastAPI/aiogram
-logging.getLogger().handlers = []  # Очищаем root logger
-logging.basicConfig(level=logging.CRITICAL)  # Отключаем почти все
+#logging.getLogger().handlers = []  # Очищаем root logger
+#logging.basicConfig(level=logging.CRITICAL)  # Отключаем почти все
 # logging.getLogger("uvicorn").disabled = True
 # logging.getLogger("uvicorn.access").disabled = True
 # logging.getLogger("aiogram").disabled = True
 logger_api = setup_logger('api', LOG_CONFIG_API)
+logger_api.getLogger("uvicorn").disabled = True
+logger_api.getLogger("uvicorn.access").disabled = True
+logger_api.getLogger("aiogram").disabled = True
 import asyncio
 import aiofiles
 import json
