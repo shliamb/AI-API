907d2b80318b23d93cc8fc358c2b1d8447f936a8
AlexProwler <first-remont@mail.ru>
2025-06-16 00:38:04 +0300

    adfd

--- app/api.py
+++ app/api.py
@@ -721,12 +721,7 @@ async def grok_api(
 
 
 if __name__ == "__main__":
-    uvicorn.run(
-        app,
-        host="0.0.0.0",
-        port=8000,
-        log_config=None
-    )
+    uvicorn.run()
 
 
 
